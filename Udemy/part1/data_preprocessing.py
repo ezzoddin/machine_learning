9b2114413018d205cdb8e824363bd490c94dd17e
ezzoddin <mobina_ezzoddin@semnan.ac.ir>
2021-03-26 23:23:48 +0430

    importing the dataset

--- Udemy/part1/data_preprocessing.py
+++ Udemy/part1/data_preprocessing.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
