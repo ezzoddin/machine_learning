8b1c2332499133b9b1d31be624da4f869b2ee5e9
ezzoddin <mobina_ezzoddin@semnan.ac.ir>
2021-03-26 23:30:34 +0430

    splitting the dataset into the training set and test set

--- Udemy/part1/data_preprocessing.py
+++ Udemy/part1/data_preprocessing.py
@@ -24,3 +24,11 @@ from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
 print(y)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
